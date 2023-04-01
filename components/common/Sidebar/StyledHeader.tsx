a04023d73f7502e46d8f8ff117d0bc5e62db4618
toylentoy <lentoytoy@gmail.com>
2023-04-01 21:44:05 +0300

    2 page article component sibebar component styles updated

--- components/common/Sidebar/StyledHeader.tsx
+++ components/common/Sidebar/StyledHeader.tsx
@@ -10,7 +10,7 @@ export const StyledHeader = styled(Box)(({ theme }) => ({
   borderRadius: "0.75rem",
   padding: "1rem 1.5rem",
   boxShadow:
-    "#00000024 0rem 0.25rem 1.25rem 0rem, rgba(30, 30, 54, 0.5) 0rem 0.4375rem 0.625rem -0.3125rem",
+    "#00000024 0rem 0.25rem 1.25rem 0rem, rgba(30, 30, 54, 0.4) 0rem 0.4375rem 0.625rem -0.3125rem",
   color: "white",
   fontSize: "22px",
   lineHeight: "22px",
