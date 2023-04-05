ddd8390af9410aafd1c89f3d58c640653f586203
toylentoy <lentoytoy@gmail.com>
2023-04-05 15:57:05 +0300

    text tooltip: added simple string splitting by whitespace formater in useMemo hook

--- components/ui/Tooltip/Tooltip.tsx
+++ components/ui/Tooltip/Tooltip.tsx
@@ -13,27 +13,41 @@ type Props = {
 const Tooltip = ({
   children: trigger,
   placement,
-  title,
+  title: inTitle,
   inline,
   delay,
 }: Props) => {
-  if (title && typeof title === "string") {
-    if (title.length > 22) {
-      let prevIndex = 0;
-      let newTitle: string = "";
-      do {
-        const nextIndex = title.indexOf(" ", prevIndex + 20);
-        if (nextIndex >= 0) {
-          newTitle += title.substring(prevIndex, nextIndex + 1) + "\n";
-          prevIndex = nextIndex + 1;
-        } else {
-          newTitle += title.substring(prevIndex);
-          prevIndex = -1;
-          break;
-        }
-      } while (prevIndex >= 0);
-      title = newTitle.replaceAll(". ", ". \n");
+  const title = React.useMemo(() => {
+    let title = inTitle;
+    if (inTitle && typeof inTitle === "string") {
+      if (inTitle.length >= 27) {
+        let title = inTitle;
+        let prevIndex = 0;
+        let newTitle: string = "";
+        do {
+          const nextIndex = title.indexOf(" ", prevIndex + 20);
+          let addingSubstring = "";
+          if (nextIndex >= 0) {
+            addingSubstring = title.substring(prevIndex, nextIndex + 1) + "\n";
+            prevIndex = nextIndex + 1;
+          } else {
+            addingSubstring = title.substring(prevIndex);
+            prevIndex = -1;
+          }
+          newTitle += addingSubstring.replace(/[^\s]{27}/gim, (matched) => {
+            return matched + "-\n";
+          });
+        } while (prevIndex >= 0);
+        title = newTitle
+          .replaceAll(". ", ". \n")
+          .replace(/[\r\n]\s*[\r\n]/gim, "\n")
+          .trim();
+        return title;
+      }
     }
+    return title;
+  }, [inTitle]);
+  if (title && typeof title === "string") {
     return (
       <StyledTooltip
         data-tooltip={placement || "down" + (delay ? delay.toString() : "")}
