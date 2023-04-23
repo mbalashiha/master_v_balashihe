47eec847ffc73a7da3787f81e7b6f2da4a20510f
toylentoy <lentoytoy@gmail.com>
2023-04-23 18:28:03 +0300

    small json file added with some cache

--- const/contacts.ts
+++ const/contacts.ts
@@ -1,4 +1,5 @@
-
-export const phoneNumber = "+7 (926) 212-12-55";
-export const locationPlace = "Балашиха, центр города";
-export const email = "mastervbalashihe@mail.ru";
+export const phoneNumber: string =
+  process.env["NEXT_PUBLIC_CONTACT_PHONE_NUMBER_CONTRY"] || "";
+export const locationPlace: string =
+  process.env["NEXT_PUBLIC_LOCATION_PLACE"] || "";
+export const email: string = process.env["NEXT_PUBLIC_CONTACT_EMAIL"] || "";
--- start-cms.sh
+++ start-cms.sh
@@ -6,6 +6,7 @@ kill -9 $(lsof -t -i:3000) &> /dev/null
 kill -9 $(lsof -t -i:4402) &> /dev/null
 kill -9 $(lsof -t -i:9229) &> /dev/null
 kill -9 $(lsof -t -i:9934) &> /dev/null
-parallel --ungroup --retries 0 --halt now,fail=1 ::: 'sudo service mariadb start' 'cd ~/cms/master_v_balashihe_graphql_api && code .' 'cd ~/cms/master_v_balashihe && sleep 0.3 && code .' 'cd ~/cms/master_v_balashihe_graphql_api && pnpm start:dev' 'cd ~/cms/master_v_balashihe && pnpm dev'
+parallel --ungroup --retries 0 --halt now,fail=1 ::: 'cd ~/cms/master_v_balashihe_graphql_api && code .' 'cd ~/cms/master_v_balashihe && sleep 0.3 && code .' 'cd ~/cms/master_v_balashihe_graphql_api && pnpm start:dev' 'cd ~/cms/master_v_balashihe && pnpm dev'
+
 
 
