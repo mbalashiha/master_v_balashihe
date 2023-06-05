be8a47f85bf19517cde926a89a0c7b47b1ddec75
toylentoy <lentoytoy@gmail.com>
2023-06-05 23:25:40 +0300

    rsync.sh bash script have not used corrent PATH enviroment via ssh login, so I have hardcoded PATH enviroment variable intossh commands here-doc

--- rsync.sh
+++ rsync.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 rsync -avv --progress --password-file=/home/yoo/rsync_pass /home/yoo/cms/ rsync_master@192.168.0.50::cms
-ssh 'safeuser@192.168.0.50' << EOF
-cd /var/www/cms/master_v_balashihe && /home/safeuser/.local/share/pnpm/pnpm install;
-cd /var/www/cms/master_v_balashihe_graphql_api && /home/safeuser/.local/share/pnpm/pnpm install;
-cd /var/www/cms/master_v_balashihe_graphql_api && /home/safeuser/.local/share/pnpm/pnpm build;
-chown -R safeuser /var/www/cms;
-chmod -R 744 /var/www/cms;
-EOF
+ssh 'safeuser@192.168.0.50' -t <<-'ENDSSH'
+export PATH=/home/safeuser/.local/share/pnpm:/home/safeuser/.nvm/versions/node/v18.16.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+cd /var/www/cms/master_v_balashihe && pnpm install
+cd /var/www/cms/master_v_balashihe_graphql_api && pnpm install
+echo 'compiling graphql-api backend now...'
+cd /var/www/cms/master_v_balashihe_graphql_api && pnpm build
+echo 'compiling of graphql-api backend finished.'
+chown -R safeuser /var/www/cms
+chmod -R 744 /var/www/cms
+ENDSSH
 rsync -a --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe/public/ /home/yoo/cms/master_v_balashihe/public
 rsync -a --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe_graphql_api/mysql-full-dump/ /home/yoo/cms/master_v_balashihe_graphql_api/mysql-full-dump
