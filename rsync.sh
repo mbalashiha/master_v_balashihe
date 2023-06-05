32af2874f44731a30ce1d78b68854ba59c2e219e
toylentoy <lentoytoy@gmail.com>
2023-06-05 17:55:56 +0300

    added rsyncd.conf file to repository, added chmod commands to rsync.sh script

--- rsync.sh
+++ rsync.sh
@@ -5,6 +5,8 @@ ssh 'safeuser@192.168.0.50' << EOF
 cd /var/www/cms/master_v_balashihe && /home/safeuser/.local/share/pnpm/pnpm install;
 cd /var/www/cms/master_v_balashihe_graphql_api && /home/safeuser/.local/share/pnpm/pnpm install;
 cd /var/www/cms/master_v_balashihe_graphql_api && /home/safeuser/.local/share/pnpm/pnpm build;
+chown -R safeuser /var/www/cms;
+chmod -R 744 /var/www/cms;
 EOF
-rsync -a --progress --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe/public/ /home/yoo/cms/master_v_balashihe/public
-rsync -a --progress --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe_graphql_api/mysql-full-dump/ /home/yoo/cms/master_v_balashihe_graphql_api/mysql-full-dump
+rsync -a --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe/public/ /home/yoo/cms/master_v_balashihe/public
+rsync -a --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe_graphql_api/mysql-full-dump/ /home/yoo/cms/master_v_balashihe_graphql_api/mysql-full-dump
