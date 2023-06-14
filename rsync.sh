9261c638644bdef3e52aa7192a4870c5df7369f9
toylentoy <lentoytoy@gmail.com>
2023-06-15 00:15:16 +0300

    rsync shell script: added chmod for separate folder for nginx

--- rsync.sh
+++ rsync.sh
@@ -9,7 +9,13 @@ echo 'compiling graphql-api backend now...'
 cd /var/www/cms/master_v_balashihe_graphql_api && pnpm build
 echo 'compiling of graphql-api backend finished.'
 chown -R safeuser /var/www/cms
-chmod -R 755 /var/www/cms
+chmod -R 700 /var/www/cms
+chmod u=rwx,go=rX /var/www/cms
+chmod u=rwx,go=rX /var/www/cms/master_v_balashihe
+chmod u=rwx,go=rX /var/www/cms/master_v_balashihe/.next
+chmod -R u=rwx,go=rX /var/www/cms/master_v_balashihe/public
+chmod -R u=rwx,go=rX /var/www/cms/master_v_balashihe/.next/static
+pm2 kill; pm2 start /home/safeuser/ecosystem.config.js;
 ENDSSH
 rsync -a --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe/public/ /home/yoo/cms/master_v_balashihe/public
 ###rsync -a --password-file=/home/yoo/rsync_pass rsync_master@192.168.0.50::cms/master_v_balashihe_graphql_api/mysql-full-dump/ /home/yoo/cms/master_v_balashihe_graphql_api/mysql-full-dump
