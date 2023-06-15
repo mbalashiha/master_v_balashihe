525c41a5048f2bdcbae88e2e05fca8f21c66d2b9
toylentoy <lentoytoy@gmail.com>
2023-06-15 21:32:19 +0300

    removed experimental setting from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  experimental: {
-    concurrentFeatures: true,
-    serverComponents: true,
-    appDir: true,
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
