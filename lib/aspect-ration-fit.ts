a272c3d2a2998d6398693c531fbb917470516dad
toylentoy <lentoytoy@gmail.com>
2023-06-27 14:04:08 +0300

    Enhanced Next.js Image: image rounding width and height has been changed to fraction -5 floor.

--- lib/aspect-ration-fit.ts
+++ lib/aspect-ration-fit.ts
@@ -2,22 +2,22 @@
  * @param num The number to round
  * @param precision The number of decimal places to preserve
  */
-export function millisRound(num: number, precision: number = 3) {
+export function fractionFloor(num: number, precision: number = 5) {
   precision = Math.pow(10, precision);
-  return Math.round(num * precision) / precision;
+  return Math.floor(num * precision) / precision;
 }
 export function calculateAspectRatioFit(
   srcWidth: number,
   srcHeight: number,
   maxWidth: number,
   maxHeight: number
-): { width: number; height: number; srcWidth: number; srcHeight: number; } {
+): { width: number; height: number; srcWidth: number; srcHeight: number } {
   srcWidth = srcWidth || maxWidth;
   srcHeight = srcHeight || maxHeight;
   const ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
   return {
-    width: millisRound(srcWidth * ratio),
-    height: millisRound(srcHeight * ratio),
+    width: fractionFloor(srcWidth * ratio),
+    height: fractionFloor(srcHeight * ratio),
     srcWidth,
     srcHeight,
   };
@@ -26,29 +26,30 @@ export function fitWidth(
   srcWidth: number,
   srcHeight: number,
   maxWidth: number
-): { width: number; height: number, srcWidth: number, srcHeight: number, } {
+): { width: number; height: number; srcWidth: number; srcHeight: number } {
   if (!srcWidth) {
     throw new Error("fit width: no image source width!");
   }
   const ratio = maxWidth / srcWidth;
   return {
-    width: millisRound(srcWidth * ratio),
-    height: millisRound(srcHeight * ratio),
-    srcWidth, srcHeight,
+    width: fractionFloor(srcWidth * ratio),
+    height: fractionFloor(srcHeight * ratio),
+    srcWidth,
+    srcHeight,
   };
 }
 export function fitHeight(
   srcWidth: number,
   srcHeight: number,
   maxHeight: number
-): { width: number; height: number; srcWidth: number; srcHeight: number; } {
+): { width: number; height: number; srcWidth: number; srcHeight: number } {
   if (!srcHeight) {
     throw new Error("fit height: no image source height!");
   }
   const ratio = maxHeight / srcHeight;
   return {
-    width: millisRound(srcWidth * ratio),
-    height: millisRound(srcHeight * ratio),
+    width: fractionFloor(srcWidth * ratio),
+    height: fractionFloor(srcHeight * ratio),
     srcWidth,
     srcHeight,
   };
