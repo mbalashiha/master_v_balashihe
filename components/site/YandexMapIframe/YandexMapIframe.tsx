2c4edf95024687495cd0f041a7e704e78eca281b
toylentoy <lentoytoy@gmail.com>
2023-07-05 14:13:35 +0300

    css responsive values added for site Footer and contacts with Map

--- components/site/YandexMapIframe/YandexMapIframe.tsx
+++ components/site/YandexMapIframe/YandexMapIframe.tsx
@@ -21,6 +21,8 @@ const YandexMapIframe = () => {
           },
           position: "relative",
           marginBottom: { xs: 0, xl: "30px" },
+          display: { xs: "block", md: "flex", lg: "block" },
+          "& > *": {},
         }}
       >
         <Container
@@ -43,20 +45,30 @@ const YandexMapIframe = () => {
               zIndex: 2,
               position: { xs: "inherit", lg: "absolute" },
               top: 0,
-              marginBottom: { xs: "15px", md: 0 },
-              minHeight: "440px",
+              marginBottom: 0,
+              height: { xs: "auto", md: "100%", lg: "auto" },
+              minHeight: { xs: "440px", md: "100%", lg: "440px" },
               width: { xs: "100%", lg: "420px" },
               marginLeft: { lg: "-15px", xl: "-55px" },
-              marginTop: { md: "43px" },
-              border: "12px solid",
-              borderColor: "background.paper",
-              borderRadius: (theme) => theme.shape.borderRadius - 5 + "px",
+              marginTop: { xs: "43px", md: 0, lg: "43px" },
+              border: (theme) => `12px solid ${theme.palette.background.paper}`,
+              borderBottomWidth: { xs: "3px", md: "10px", lg: "12px" },
+              borderRightWidth: { xs: "12px", md: 0, lg: "12px" },
+              borderRadius: {
+                xs: 0,
+                lg: "24px",
+              },
+              boxShadow: {
+                xs: "none",
+                lg: `0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)`,
+              },
             }}
           >
             <Box
               sx={{
                 width: "100%",
-                minHeight: "440px",
+                height: { xs: "auto", md: "100%", lg: "auto" },
+                minHeight: { xs: "440px", md: "100%", lg: "440px" },
                 border: "3px solid",
                 borderColor: "primary.light",
                 borderRadius: (theme) => theme.shape.borderRadius - 9 + "px",
@@ -103,8 +115,7 @@ const YandexMapIframe = () => {
             maxWidth: "1760px",
             marginLeft: "auto",
             marginRight: "auto",
-            border: "11px solid",
-            borderColor: (theme) => theme.palette.background.paper,
+            border: (theme) => `11px solid ${theme.palette.background.paper}`,
             borderRadius: { xs: 0, xl: "20px" },
             position: "relative",
             overflow: "hidden",
