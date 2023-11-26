0457257c59d835bee83d61a8ec3ccb327ff104b3
toylentoy <lentoytoy@gmail.com>
2023-11-26 22:45:55 +0300

    added some ticker landing images, component BreaksBySymbol added necessary react key index

--- components/ui/BreaksBySymbol.tsx
+++ components/ui/BreaksBySymbol.tsx
@@ -7,27 +7,35 @@ interface Props {
 export function BreaksBySymbol(props: Props) {
   const character = props.character || ",";
   const { children } = props;
+  let counter = 0;
   return (
     <>
       {React.Children.map(children, (str) => {
+        counter++;
         if (typeof str === "string") {
           return (
-            <>
+            <React.Fragment key={counter}>
               {str.split(character).map((el, ind, array) => (
-                <>
+                <React.Fragment key={ind.toString()}>
                   {el.trim()}
                   {ind < array.length - 1 && (
                     <>
                       {character} <br />
                     </>
                   )}
-                </>
+                </React.Fragment>
               ))}
-            </>
+            </React.Fragment>
+          );
+        } else if (str) {
+          return (
+            <BreaksBySymbol key={counter} character={character}>
+              {str}
+            </BreaksBySymbol>
           );
         } else {
-          return <BreaksBySymbol character={character}>{str}</BreaksBySymbol>;
-        };
+          return null;
+        }
       })}
     </>
   );
